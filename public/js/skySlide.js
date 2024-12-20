2ecd4a3775808a79a5562288c130d2998b4f352c
yyyn012 <yyyn12_@naver.com>
2024-12-20 21:48:44 +0900

    Update moon & star

--- public/js/skySlide.js
+++ public/js/skySlide.js
@@ -1,24 +1,23 @@
 const blue = document.querySelector(".blue");
 const star = document.querySelectorAll(".ico_star");
+const on = document.querySelector(".on");
 
 const moonOriginal = document.querySelector(".moon_original");
 const moonShadow = document.querySelector(".moon_shadow");
 
-const on = document.querySelector(".on");
 const night = document.querySelector(".night");
 
 const intersectionObserver = new IntersectionObserver(function (entries) {
   // intersectionRatio가 0이라는 것은 대상을 볼 수 없다는 것이므로 아무것도 하지 않음
   if (entries[0].intersectionRatio <= 0) return;
 
-  // blue.style.height = "0";
-  // star.classList.add("on");
+  blue.style.height = "0";
 
-  // star.forEach(() => {
-  //   for (let i = 0; i < star.children.length; i++) {
-  //     star.children[i].classList.add("on");
-  //   }
-  // });
+  star.forEach(() => {
+    for (let i = 0; i < star.length; i++) {
+      star[i].classList.add("on");
+    }
+  });
 
   moonShadow.classList.add("night");
   moonOriginal.classList.add("night");
